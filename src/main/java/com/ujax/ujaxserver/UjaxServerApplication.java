48c11eadf5985ce124ad9ac7e4958ad49101b9e3
kwongwangjae <kgj9906@naver.com>
2026-01-22 12:58:52 +0900

    init: spring boot project setup

---
+++ src/main/java/com/ujax/ujaxserver/UjaxServerApplication.java
@@ -0,0 +1,13 @@
+package com.ujax.ujaxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjaxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UjaxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujax/ujaxserver/UjaxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujax.ujaxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjaxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
