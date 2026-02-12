478c876054b60a572871c0fc0b440c5e1b58cd29
jinukee <jinukee333@gmail.com>
2026-02-12 10:54:57 +0900

    feat : add endpoint for polling(test case result)

--- src/main/java/com/ujax/infrastructure/web/submission/SubmissionController.java
+++ src/main/java/com/ujax/infrastructure/web/submission/SubmissionController.java
@@ -3,18 +3,21 @@ package com.ujax.infrastructure.web.submission;
 import com.ujax.application.submission.SubmissionService;
 import com.ujax.infrastructure.web.submission.dto.SubmissionRequest;
 import com.ujax.infrastructure.web.submission.dto.SubmissionResponse;
+import com.ujax.infrastructure.web.submission.dto.SubmissionResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/workspaces/{workspaceId}/problems/{problemId}")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class SubmissionController {
 
     private final SubmissionService submissionService;
 
-    @PostMapping("/submissions")
+    @PostMapping("/workspaces/{workspaceId}/problems/{problemId}/submissions")
     public ResponseEntity<SubmissionResponse> createSubmission(
             @PathVariable Long workspaceId,
             @PathVariable Long problemId,
@@ -23,5 +26,15 @@ public class SubmissionController {
         String unifiedToken = submissionService.submitAndAggregateTokens(request);
 
         return ResponseEntity.ok(SubmissionResponse.SubmissionData.ok(unifiedToken));
+    }
+
+    @GetMapping("/submissions/{submissionToken}")
+    public ResponseEntity<SubmissionResultResponse> getResults(
+            @PathVariable String submissionToken) {
+
+        List<SubmissionResultResponse.TestCaseResult> results =
+                submissionService.getSubmissionResults(submissionToken);
+
+        return ResponseEntity.ok(SubmissionResultResponse.ok(results));
     }
 }
