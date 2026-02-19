bb8b958e2e7913f8cfc712c0d2cf367f9cb66419
kwongwangjae <kgj9906@naver.com>
2026-02-19 19:39:57 +0900

    feat etitiy set

---
+++ src/main/java/com/ujax/domain/problem/WorkspaceProblem.java
@@ -0,0 +1,63 @@
+package com.ujax.domain.problem;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.SQLDelete;
+
+import com.ujax.domain.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "workspace_problem")
+@Filter(
+	name = "softDeleteFilter",
+	condition = "deleted_at IS NULL"
+)
+@SQLDelete(sql = "UPDATE workspace_problem SET deleted_at = now() WHERE id = ?")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WorkspaceProblem extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "problem_box_id", nullable = false)
+	private ProblemBox problemBox;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "problem_id", nullable = false)
+	private Problem problem;
+
+	@Column(nullable = false)
+	private LocalDateTime deadline;
+
+	private LocalDateTime scheduledAt;
+
+	private WorkspaceProblem(ProblemBox problemBox, Problem problem, LocalDateTime deadline,
+		LocalDateTime scheduledAt) {
+		this.problemBox = problemBox;
+		this.problem = problem;
+		this.deadline = deadline;
+		this.scheduledAt = scheduledAt;
+	}
+
+	public static WorkspaceProblem create(ProblemBox problemBox, Problem problem, LocalDateTime deadline,
+		LocalDateTime scheduledAt) {
+		return new WorkspaceProblem(problemBox, problem, deadline, scheduledAt);
+	}
+}
