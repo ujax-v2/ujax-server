48b43a5f59e5812177fdbac220f7f573db3a448e
kwongwangjae <121513336+kwongwangjae@users.noreply.github.com>
2026-03-30 10:48:34 +0900

    언어불일치 해결 (#67)

--- src/test/java/com/ujax/domain/solution/ProgrammingLanguageTest.java
+++ src/test/java/com/ujax/domain/solution/ProgrammingLanguageTest.java
@@ -2,11 +2,15 @@ package com.ujax.domain.solution;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
 
 class ProgrammingLanguageTest {
 
@@ -50,6 +54,13 @@ class ProgrammingLanguageTest {
 		assertThat(ProgrammingLanguage.fromLanguage(language)).isEqualTo(expected);
 	}
 
+	@ParameterizedTest
+	@MethodSource("baekjoonModifiedLanguages")
+	@DisplayName("백준 수정 표기와 NBSP가 섞인 언어 문자열도 매핑한다")
+	void fromLanguageWithModifiedSuffix(String language, ProgrammingLanguage expected) {
+		assertThat(ProgrammingLanguage.fromLanguage(language)).isEqualTo(expected);
+	}
+
 	@Nested
 	@DisplayName("fromLanguage가 OTHER를 반환하는 경우")
 	class FromLanguageOther {
@@ -71,5 +82,14 @@ class ProgrammingLanguageTest {
 		void unknownLanguage() {
 			assertThat(ProgrammingLanguage.fromLanguage("Haskell")).isEqualTo(ProgrammingLanguage.OTHER);
 		}
+	}
+
+	private static Stream<Arguments> baekjoonModifiedLanguages() {
+		return Stream.of(
+			Arguments.of("C99\u00A0/\u00A0수정", ProgrammingLanguage.C),
+			Arguments.of("C++17\u00A0/\u00A0수정", ProgrammingLanguage.CPP),
+			Arguments.of("C# 9.0\u00A0/\u00A0수정", ProgrammingLanguage.CSHARP),
+			Arguments.of("node.js\u00A0/\u00A0수정", ProgrammingLanguage.JAVASCRIPT)
+		);
 	}
 }
